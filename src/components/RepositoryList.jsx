b8a56b0b67b9342f78462b4af8ed0b16ffa92234
Vitor <vitorduran88@gmail.com>
2022-07-17 15:12:57 -0300

    componentes e propriedades

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/',
+
+}
+
+const repositoryName = 'unform2';
+
+
+export function RepositoryList(){
+    return ( 
+        <section className="repository-List">
+            <h1>Lista de repositorios</h1>
+
+            <ul>
+                <RepositoryItem repository= {repository}/>
+                <RepositoryItem repository= {repository}/>
+                <RepositoryItem repository= {repository}/>
+                <RepositoryItem repository= {repository}/>
+            </ul>
+        </section>
+    )
+}
