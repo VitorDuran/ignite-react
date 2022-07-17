8ab73b0aad87ca1e0542f85c70d59c15ff819f79
Vitor <vitorduran88@gmail.com>
2022-07-17 08:55:55 -0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { render } from 'react-dom';
+import { App } from './App';
+
+
+// Primeiro parametro "(h1)" é oq eeu qeuro ver em tela
+// Segundo parametro dentro de qual elemento eu quero renderizar essa informação
+
+render(<App />, document.getElementById('root'))
